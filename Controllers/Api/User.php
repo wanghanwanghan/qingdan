378c2ea4335bcd95241e8672859ddd27abf211d7
wanghan <minglongoc@me.com>
2021-04-26 17:18:36 +0800

    2021-04-26_17:18:36

--- Controllers/Api/User.php
+++ Controllers/Api/User.php
@@ -12,6 +12,10 @@ class User extends ControllerBase
     {
         $jsCode = Request::getInstance()->getRequestData('jsCode');
 
+        if (empty($jsCode)) {
+            return $this->writeJson(201, null, null, 'jsCode不能是空');
+        }
+
         $res = WeiXinService::getInstance()->getOpenId($jsCode);
 
         return $this->writeJson(200, null, $res);
